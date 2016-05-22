e47138790f98eb63902a1b5f15b89986505a67ab
keligijus <k.eligijus@gmail.com>
2016-05-22 19:09:05 +0300

    frosted glass effect

--- app/assets/js-source/prepTemplate.js
+++ app/assets/js-source/prepTemplate.js
@@ -17,17 +17,20 @@ var infApp = infApp || {};
                     : shot.images.normal;
 
     t += '<figure class="shot' + favourite + '" id="shotId-' + shot.id + '">' +
+            '<style>#shotId-' + shot.id + ' .shot-overlay:before { background-image: url(' + imageUrl + '); }</style>' +
             '<figcaption class="shot-overlay">' +
-              '<div class="shot-overlay-text">' +
-                '<h2 class="shot-title">' +
-                  '<a href="' + shot.html_url + '" target="_blank">' + shot.title + '</a>' +
-                '</h2>' +
-                '<hr>' +
-                '<a class="shot-author" href="' + shot.user.html_url + '" target="_blank">' + shot.user.name + '</a>' +
-                '<span class="favourite-heart"></span>' +
-              '</div>' +
-              '<div class="shot-overlay-cta">' +
-                '<button onclick="infApp.favourites.toggle(' + shot.id + ')">Favourite</button>' +
+              '<div class="shot-overlay-inner">' +
+                '<div class="shot-overlay-text">' +
+                  '<h2 class="shot-title">' +
+                    '<a href="' + shot.html_url + '" target="_blank">' + shot.title + '</a>' +
+                  '</h2>' +
+                  '<hr>' +
+                  '<a class="shot-author" href="' + shot.user.html_url + '" target="_blank">' + shot.user.name + '</a>' +
+                  '<span class="favourite-heart"></span>' +
+                '</div>' +
+                '<div class="shot-overlay-cta">' +
+                  '<button onclick="infApp.favourites.toggle(' + shot.id + ')">Favourite</button>' +
+                '</div>' +
               '</div>' +
             '</figcaption>' +
             '<img data-src="' + imageUrl + '" alt="' + shot.title + '">' +
