5bd13ac88679177117397bd78e046ee0313f85f4
keligijus <k.eligijus@gmail.com>
2016-05-22 06:29:39 +0300

    cleanup

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -2,16 +2,15 @@
 
 var infApp = infApp || {};
 
-(function(){
-
-infApp.settings = {
-  api: {
-    baseUrl: 'https://api.dribbble.com/v1',
-    access_token: '3402bc128ab90bdc0d1ddab729be14c034154ba0fdca8bd1e0d8f230cac3d82c'
-  }
-};
-
+(function () {
+  infApp.settings = {
+    api: {
+      baseUrl: 'https://api.dribbble.com/v1',
+      access_token: '3402bc128ab90bdc0d1ddab729be14c034154ba0fdca8bd1e0d8f230cac3d82c'
+    }
+  };
 }());
+
 'use strict';
 
 var infApp = infApp || {};
@@ -244,33 +243,6 @@ var infApp = infApp || {};
 
 
 }());
-(function(){
- console.log('I can use es6 magic now!');
-
-  var apiUrl = 'https://api.dribbble.com/v1'
-  var accessToken = "3402bc128ab90bdc0d1ddab729be14c034154ba0fdca8bd1e0d8f230cac3d82c";
-
-  var request = new XMLHttpRequest();
-  request.open('GET', apiUrl + '/shots/?access_token=' + accessToken, true);
-
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
-      // Success!
-      var data = JSON.parse(request.responseText);
-          console.log(data);
-    } else {
-      // We reached our target server, but it returned an error
-
-    }
-  };
-
-  request.onerror = function() {
-    // There was a connection error of some sort
-  };
-
-  request.send();
-
-} ());
 'use strict';
 
 var infApp = infApp || {};
